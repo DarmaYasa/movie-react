74e94437b9e16b8782195f55f45f0a0713128734
Darma Yasa <yasa13darma@gmail.com>
2024-04-13 16:24:32 +0800

    move api key to env

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,28 +1,31 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import Layout from "../Layout/Layout";
 import "./Home.css";
 import MovieCard from "../MovieCard";
 
 const HomePage = () => {
   const [movies, setMovies] = useState([]);
-  const apiKey =
-    "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMTA2ZjA1NmY1N2IwOWE0YjNlMjJkZDk5NzQyMWFjMyIsInN1YiI6IjVkODhjN2ZmYjc2Y2JiMDAxZmU0NTM0ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.gYhpRjxbqfXaXbuxKgPkvw9gcpOMfZU6U7cevl9ol7Y";
 
-  const url =
-    "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc";
-  const options = {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-      Authorization: `Bearer ${apiKey}`,
-    },
-  };
+  const fetchMovies = () => {
+    const apiKey = import.meta.env.VITE_MOVIE_API_KEY;
+    const url =
+      "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc";
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${apiKey}`,
+      },
+    };
 
-  useEffect(() => {
     fetch(url, options)
       .then((res) => res.json())
       .then((json) => setMovies(json.results))
       .catch((err) => console.error("error:" + err));
+  };
+
+  useEffect(() => {
+    fetchMovies();
   }, []);
 
   return (
